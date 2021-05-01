b7c46191cefdeedf3e7be3ccfc79a5406f005e87
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2021-05-01 22:04:40 +0200

    Creato il costruttore di Write che inizializza il file data.csv

--- Write.py
+++ Write.py
@@ -1,5 +1,17 @@
 from setup import *
+import csv
 
 
 class Write:
-    pass
+
+    def __init__(self, path):
+        # crea il file se non esiste e lo apre inizializzando i campi
+        with open(path + "\data.csv", 'w', newline='') as file:
+
+            header = ["ID", "Partita", "Quota", "Punteggio al 75'", "Punteggio Finale"]
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(header)
+
+            # usa per scrivere riga, come parametri prende i dettagli della scommessa
+            # writer.writerow()
+
